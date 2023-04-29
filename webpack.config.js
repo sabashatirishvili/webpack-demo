a6cca090675c55cedbd1b504919a7288e3a03ca4
Saba Shatirishvili <shatirishvilis1@gmail.com>
2023-04-29 21:01:12 +0400

    HtmlWebpackPlugin added

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,15 @@ module.exports = {
 		index:'./src/index.js',
 		print:'./src/print.js',
 	},
+	plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
 	output: {
 		filename: "[name].bundle.js",
 		path: path.resolve(__dirname, 'dist'),
+		clean: true,
 	},
 
 };
