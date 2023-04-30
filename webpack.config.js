06829b8c8770722617204bb3fbae437d1914caa5
Saba Shatirishvili <shatirishvilis1@gmail.com>
2023-04-30 20:17:04 +0400

    Addedd devServer

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,10 @@ module.exports = {
 		index:'./src/index.js',
 		print:'./src/print.js',
 	},
+	devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
 	plugins: [
     new HtmlWebpackPlugin({
       title: 'Output Management',
@@ -16,6 +20,9 @@ module.exports = {
 		filename: "[name].bundle.js",
 		path: path.resolve(__dirname, 'dist'),
 		clean: true,
+		publicPath: '/',
 	},
-
+	optimization: {
+    runtimeChunk: 'single',
+  },
 };
